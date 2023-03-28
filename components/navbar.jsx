ed9e9dbbacec76be1e6afabc817fc2a69bd6a919
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2023-03-27 18:06:40 -0700

    Added a close on click on Hamburger menu and fixed the positioning to flex for bm-items.

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,11 +3,23 @@ import Link from 'next/link';
 import { slide as Menu } from 'react-burger-menu'
 import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 
 
 export default function Navbar(){
 	const router = useRouter
 	const { user, logout } =useAuth();
+	const [isMenuOpen, handleMenu] = useState(false);
+
+  const handleCloseMenu = () => {
+    handleMenu(false);
+  };
+
+  const handleStateChange = (state) => {
+    handleMenu(state.isOpen);
+  };
+
 	const menuitems = [
 
 		{
@@ -32,9 +44,28 @@ export default function Navbar(){
 		},
 	];
 
+	const hamburgerMenu = [
+	{
+		title: 'Home',
+		path: '/',
+		uniqueId: '1',
+	},
+	{
+		title: 'About',
+		path: '',
+		uniqueId: '2',
+	},
+	{
+		title: 'Contact',
+		path: '',
+		uniqueId: '3',
+	},
+
+	]
+
 
 	return( 
-		<header className="hidden flex justify-between items-center p-5 bg-gray-50">
+		<header className="flex justify-between items-center p-5 bg-gray-50">
 			<div className="flex w-full lg:w-auto items-center">
         <Link href="/" className="logo">
 					<span className="font-bold text-blue1">Beyond</span>
@@ -57,10 +88,21 @@ export default function Navbar(){
 			</div>
 			<div>
 			<div className='lg:hidden'>
-			<Menu right>
-			<Link id="home" className="menu-item" href="/">Home</Link>
+			<Menu right isOpen={isMenuOpen} onStateChange={handleStateChange}>
+				{
+					hamburgerMenu.map((item) => (
+						<Link key={item.uniqueId}
+						id={item.title}
+						className="menu-item" 
+						href={item.path} 
+						onClick={() => handleCloseMenu()}>{item.title}</Link>
+					))
+				}
+
+			{/* <Link id="home" className="menu-item" href="/" onClick={() => handleCloseMenu()}>Home</Link>
 			<Link id="about" className="menu-item" href="#">About</Link>
-			<Link id="contact" className="menu-item" href="#">Contact</Link>
+			<Link id="contact" className="menu-item" href="#">Contact</Link> */}
+			
 			{user  ? (
 				<Link className="menu-item--small" href="/login" onClick={() => {logout()}}>Logout</Link>	
 				) : <Link className="menu-item--small" href="/signup">Log In/Sign Up</Link> 
